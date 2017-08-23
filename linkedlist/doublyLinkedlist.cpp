6b2700fdbd72e46cafe3a2b3717d26f748cf9194
ram2509 <ramchaurasiya2509@gmail.com>
2017-08-23 14:48:28 -0400

    add delete funtion

--- linkedlist/doublyLinkedlist.cpp
+++ linkedlist/doublyLinkedlist.cpp
@@ -47,6 +47,74 @@ void appendNodeAtFront(node*& head,int data){
        head = temp;
 }
 
+///insert node at specific position
+void appendNodeAtPosition(node*& head, int data, int position){
+    node *temp = head;
+    int i = 0;
+    for(i=0;i<position;i++){
+        temp=temp->next;
+        if(temp==NULL){
+            cout<<"The number of node present in the linkedList is less given position"<<endl;
+        }
+    }
+    temp=temp->prev;
+    node *n = new node(data);
+    n->next=temp->next;
+    n->prev=temp;
+    temp->next->prev = n;
+    temp->next = n;
+    return;
+}
+
+///delete the node
+void deleteNode(node*& head, int key){
+   node *temp = head;
+   node *old;
+   while(temp!=NULL){
+      if(temp->data==key){
+         ///key present at first node
+         if(temp==head){
+            head=head->next;
+            return;
+         }
+         else{
+            ///key present at last node
+            if(temp->next==NULL){
+            old = temp->prev;
+            old->next=NULL;
+            }
+            ///key present at intermediate node
+            else{
+                old=temp->prev;
+                old->next=temp->next;
+                temp->next->prev=old;
+            }
+         }
+
+         delete(temp);
+      }
+      temp=temp->next;
+   }
+}
+///delete node
+void takeKeyToDeleteNode(node*& head){
+     cout<<"Enter the key"<<endl;
+     int key;
+     cin>>key;
+     deleteNode(head,key);
+}
+
+///insert node at specific position
+void takeInputAtPosition(node*& head){
+   cout<<"Insert node at specific position"<<endl;
+   int data,position;
+   cout<<"Enter node data"<<endl;
+   cin>>data;
+   cout<<"Enter index value"<<endl;
+   cin>>position;
+   appendNodeAtPosition(head,data,position);
+}
+
 ///insert node at front
 void takeInputAtFront(node*& head){
     cout<<"Insert node at front"<<endl;
@@ -81,6 +149,10 @@ int main(){
    takeInput(head);
    display(head);
    takeInputAtFront(head);
+   display(head);
+   takeInputAtPosition(head);
+   display(head);
+   takeKeyToDeleteNode(head);
    display(head);
    return 0;
 }
